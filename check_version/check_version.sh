dfec30487337f681313c24b8ba31d63e71f09534
Tony Ting <k999hoshing@gmail.com>
2020-07-19 19:05:59 +0800

    Add version checking scripts.

---
+++ check_version/check_version.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+REPO='/volume1/homes/ting/git_repos/docker-factorio-headless'
+
+cd $REPO || exit 1
+
+latest_version=$(/usr/bin/env python3 $REPO/check_version/get_factorio_version.py --experimental)
+
+#git clean -f && git checkout -- .
+# check git version
+version=$(sed -n -E 's|ENV VERSION (.+)|\1|p' Dockerfile)
+echo 'Current: '"$version"', latest: '"$latest_version"
+
+if [ "$version" != "$latest_version" ]; then
+    sed -E -i 's|(ENV VERSION ).+|\1'"$latest_version"'|g' Dockerfile
+    git add Dockerfile
+    git commit -m 'Version '"$latest_version"
+    git tag "$latest_version"
+    git push --follow-tags
+fi
